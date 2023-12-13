8c45536ffc71f674cd96e635c947f1a21e2027ba
cmbagay <cbabagaygm@gmail.com>
2023-12-12 21:58:24 -0700

    base code structure

---
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -0,0 +1,13 @@
+import { NavLink } from "react-router-dom";
+
+import styles from "./NavigationBar.module.css";
+
+function NavigationBar() {
+  return (
+    {
+
+    }
+  );
+}
+
+export default NavigationBar;
