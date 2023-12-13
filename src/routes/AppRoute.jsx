f0c61719172060b6044a7967df87e75c2893f2fb
cmbagay <cbabagaygm@gmail.com>
2023-12-13 00:09:39 -0700

    fix css to adhere to best practice

--- src/routes/AppRoute.jsx
+++ src/routes/AppRoute.jsx
@@ -2,8 +2,8 @@ import { Routes, Route, Navigate } from "react-router-dom";
 
 import Home from "../pages/Home/Home";
 import Spacecrafts from "../pages/Spacecrafts/Spacecrafts";
-// import SpacecraftBuild from "../pages/SpacecraftBuild/SpacecraftBuild";
-// import Spacecraft from "../pages/Spacecraft/Spacecraft";
+import SpacecraftBuild from "../pages/SpacecraftBuild/SpacecraftBuild";
+import Spacecraft from "../pages/Spacecraft/Spacecraft";
 import Planets from "../pages/Planets/Planets";
 
 function AppRoute() {
@@ -12,6 +12,8 @@ function AppRoute() {
       <Route path="/" element={<Home />} />
       <Route path="/spacecrafts" element={<Spacecrafts />} />
       <Route path="/planets" element={<Planets />} />
+      <Route path="/spacecraft/:id" element={<Spacecraft />} />
+      <Route path="/spacecraft/build" element={<SpacecraftBuild />} />
     </Routes>
   );
 }
